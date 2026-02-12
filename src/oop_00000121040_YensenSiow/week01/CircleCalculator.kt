d47c6cf8939469bb126baeb88616eb0803b30712
Yensend <siownairui@gmail.com>
2026-02-12 14:30:05 +0700

    week01: (tugas 1) clean up unused argos and comments

--- src/oop_00000121040_YensenSiow/week01/CircleCalculator.kt
+++ src/oop_00000121040_YensenSiow/week01/CircleCalculator.kt
@@ -1,6 +1,6 @@
     package oop_00000121040_YensenSiow.week01
 
-    fun main(args: Array<String>){
+    fun main(){
         val radius = 7.0
         val pi = 3.14
         var area = pi * radius * radius
