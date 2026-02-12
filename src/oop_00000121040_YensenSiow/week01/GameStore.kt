cf5ef61ae5554133298583815fba50b58ad98cb2
Yensend <siownairui@gmail.com>
2026-02-12 15:12:13 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_00000121040_YensenSiow/week01/GameStore.kt
+++ src/oop_00000121040_YensenSiow/week01/GameStore.kt
@@ -3,10 +3,21 @@ package oop_00000121040_YensenSiow.week01
 fun main (){
     val gameTitle = "Red Dead Redemtion 2"
     val price = 900000
+    val finalPrice = calculateDiscount(price)
+    printReceipt(
+        title = gameTitle,
+        finalPrice = finalPrice,
+    )
 }
 
 fun calculateDiscount(price: Int): Int =
     if (price > 500000) {
         (price * 0.8).toInt()
     } else { (price * 0.9).toInt()
+}
+
+fun printReceipt(title: String, finalPrice: Int){
+    println("nota pembayaran SteamKW")
+    println("Judul Game  : $title")
+    println("Total Bayar : Rp $finalPrice")
 }
