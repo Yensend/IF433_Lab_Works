e8e36848e3354420e38a4da99f0d6b41477ed0d7
Yensend <siownairui@gmail.com>
2026-02-12 14:56:22 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000121040_YensenSiow/week01/GameStore.kt
+++ src/oop_00000121040_YensenSiow/week01/GameStore.kt
@@ -4,3 +4,9 @@ fun main (){
     val gameTitle = "Red Dead Redemtion 2"
     val price = 900000
 }
+
+fun calculateDiscount(price: Int): Int =
+    if (price > 500000) {
+        (price * 0.8).toInt()
+    } else { (price * 0.9).toInt()
+}
