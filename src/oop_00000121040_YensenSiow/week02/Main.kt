5c93d3b51da743d323a4d61297596db83caa893d
Yensend <siownairui@gmail.com>
2026-02-13 16:09:43 +0700

    week02: implement selection logic for constructors

--- src/oop_00000121040_YensenSiow/week02/Main.kt
+++ src/oop_00000121040_YensenSiow/week02/Main.kt
@@ -2,32 +2,37 @@ package oop_00000121040_YensenSiow.week02
 
 import java.util.Scanner
 
-fun main(){
+fun main() {
     val scanner = Scanner(System.`in`)
     println(" aplikasi pmb umn")
-    print("maskan nama: ")
+    print("masukan nama: ")
     val name = scanner.nextLine()
 
-    print ("masukan NIM (wajib 5 karakter) : ")
+    print("masukan NIM (wajib 5 karakter) : ")
     val nim = scanner.nextLine()
     scanner.nextLine()
 
-    if (nim.length != 5){
+    if (nim.length != 5) {
         println("error : pendaftaran dibatalkan. nim harus 5 karakter")
 
-    }else {
-        print ("masukkan jurusan (enter jika belom tau): ")
-        val major = scanner.nextLine()
-        val s1 = if (major.isEmpty()){
-            Student(name,nim)
-        }else {
-            print("masukkan ipk awal: ")
-            val gpaInput = scanner.nextDouble()
-            Student(name, nim, major, gpaInput)
-        }
+    } else {
+        print("Pilih Jalur (1. Reguler, 2. Umum): ")
+        val type = scanner.nextInt()
+        scanner.nextLine()
+        if (type == 1) {
+            print("masukkan jurusan (enter jika belom tau): ")
+            val major = scanner.nextLine()
+            val s1 = Student(name, nim, major)
+            println("Terdaftar di: ${s1.major} dengan GPA awal ${s1.gpa}")
+            println("Status: Pendaftaran selesai.")
+        } else if (type == 2) {
+            val s2 = Student(name, nim)
+            println("Terdaftar di: ${s2.major} dengan GPA awal ${s2.gpa}")
+            println("Status: Pendaftaran selesai.")
 
-        println("Nama    : ${s1.name}")
-        println("Jurusan : ${s1.major}")
-        println("IPK     : ${s1.gpa}")
+        } else {
+            println("Pilihan ngawur, pendaftaran batal!")
+        }
     }
 }
+
