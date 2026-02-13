51c74c6c6ea828245251ec38fc117b1cb266eb30
Yensend <siownairui@gmail.com>
2026-02-13 15:40:41 +0700

    week02: implement user input and pre-validation

--- src/oop_00000121040_YensenSiow/week02/Main.kt
+++ src/oop_00000121040_YensenSiow/week02/Main.kt
@@ -5,4 +5,21 @@ import java.util.Scanner
 fun main(){
     val scanner = Scanner(System.`in`)
     println(" aplikasi pmb umn")
+    print("maskan nama: ")
+    val name = scanner.nextLine()
+
+    print ("masukan NIM (wajib 5 karakter) : ")
+    val nim = scanner.nextLine()
+    scanner.nextLine()
+
+    if (nim.length != 5){
+        println("error : pendaftaran dibatalkan. nim harus 5 karakter")
+
+    }else {
+        print ("masukkan jurusan: ")
+        val major = scanner.nextLine()
+
+        val s1 = Student(name, nim, major)
+        println("status: pendaftaran selesai.")
+    }
 }
--- src/oop_00000121040_YensenSiow/week02/Student.kt
+++ src/oop_00000121040_YensenSiow/week02/Student.kt
@@ -1,18 +1,16 @@
 package oop_00000121040_YensenSiow.week02
 
-package oop_00000121040_YensenSiow.week02
-
-class `Student.kt`(
+class Student (
     val name: String,
     val nim: String,
     var major: String
 ){
-    init{
-        if (nim.length != 5){
+    init {
+        if (nim.length != 5) {
             println("warning: objek tercipta dengan NIM ($nim) yang tidak valid")
             println("data mahasiswa $name mungkin akan bermasalah di sistem.")
-        }else {
-            println("logL objek student $name berhasil dialokasikan di memory.")
+        } else {
+            println("Log: objek student $name berhasil dialokasikan di memory.")
         }
     }
 }
