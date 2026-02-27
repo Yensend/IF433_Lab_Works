db92dd0623904b3eea87fc358736e01cab1dafc3
Yensend <siownairui@gmail.com>
2026-02-27 15:12:44 +0700

    week04: create basic Vehicle parent class

---
+++ src/oop_00000121040_YensenSiow/week04/Vehicle.kt
@@ -0,0 +1,14 @@
+package oop_00000121040_YensenSiow.week04
+
+open class Vehicle(val brand: String){
+    var speed: Int = 0
+
+    open fun accelerate(){
+        speed += 10
+        println("$brand melaju. Kecepatan: $speed km/jam")
+    }
+
+    open fun honk(){
+        println("beep beep!")
+    }
+}
