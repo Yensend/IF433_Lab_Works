e405c699b21b31ff8cb6e4dff04718715bc66912
Yensend <siownairui@gmail.com>
2026-03-06 15:21:50 +0700

    week05: (task 2) implement payment polymorphism

--- src/oop_00000121040_YensenSiow/week05/Main.kt
+++ src/oop_00000121040_YensenSiow/week05/Main.kt
@@ -27,4 +27,14 @@ fun main(){
     println("Luas Persegi (sisi 5): ${math.hitungLuas(5)}")
     println("Luas Persegi Panjang (10x5): ${math.hitungLuas(10, 5)}")
     println("Luas Lingkaran (r 7.0): ${math.hitungLuas(7.0)}")
+
+    val wallet = EWallet(50000.0)
+    val card = CreditCard(100000.0)
+
+    val paymentList: List<PaymentMethod> = listOf(wallet, card)
+
+    println("--- SISTEM PEMBAYARAN ---")
+    for (payment in paymentList) {
+        payment.processPayment(75000.0)
+    }
 }
