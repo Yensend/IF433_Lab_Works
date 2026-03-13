9a229b90ef17d5117b0c59407827629cab4c20bc
Yensend <siownairui@gmail.com>
2026-03-13 15:34:28 +0700

    week06: create Gopay and CreditCard

--- src/oop_00000121040_YensenSiow/week06/PaymentMethod.kt
+++ src/oop_00000121040_YensenSiow/week06/PaymentMethod.kt
@@ -3,3 +3,11 @@ package oop_00000121040_YensenSiow.week06
 interface PaymentMethod{
     fun pay(amount:Double)
 }
+
+class Gopay : PaymentMethod{
+    override fun pay (amount:Double) {println("processing rp$amount via gopay server")}
+}
+
+class CreditCard : PaymentMethod{
+    override fun pay(amount:Double) {println("contacting bank for rp$amount")}
+}
