315d6f5bc8e148bd57df30b224bd403e8c1f4af5
Yensend <siownairui@gmail.com>
2026-03-13 15:24:19 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000121040_YensenSiow/week06/GadgetInterfaces.kt
@@ -0,0 +1,13 @@
+package oop_00000121040_YensenSiow.week06
+
+interface Camera{
+    fun turnOn(){
+        println("lensa kamera terbuka dan sensor aktif")
+    }
+}
+
+interface Phone{
+    fun turnOn(){
+        println("sinyal seluler mencari jaringan")
+    }
+}
