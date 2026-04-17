46aeb6fb558141aa133ea39d4d15ae8e69f300ef
Yensend <siownairui@gmail.com>
2026-04-17 22:54:27 +0700

    week08: create basic UserProfile with nullable fields

--- src/oop_00000121040_YensenSiow/week07/Main.kt
+++ src/oop_00000121040_YensenSiow/week07/Main.kt
@@ -71,5 +71,5 @@ fun main(){
     processEvent(
         BattleState.GameOver("Terkena jebakan racun")
     )
-}
+
 }
---
+++ src/oop_00000121040_YensenSiow/week08/UserProfile.kt
@@ -0,0 +1,8 @@
+package oop_00000121040_YensenSiow.week08
+
+class UserProfile(
+    val name: String,
+    val email: String?,
+    val phone: String? = null
+
+)
