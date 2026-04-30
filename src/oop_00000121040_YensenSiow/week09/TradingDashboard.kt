6040b1a5a0500b552d3f9c4f78f18c3d95848f66
Yensend <siownairui@gmail.com>
2026-04-30 12:39:21 +0700

    week09: (task) chain sortedBy and map for worst performers

--- src/oop_00000121040_YensenSiow/week09/TradingDashboard.kt
+++ src/oop_00000121040_YensenSiow/week09/TradingDashboard.kt
@@ -18,4 +18,9 @@ fun main() {
             "WIN [${it.pair} - ${it.position}]: +${it.roe}% ROE (Lev: ${it.leverage}x)"
         }
     val losingTrades = closedTrades.filter { it.roe <= 0 }
+    val worstPerformersString = losingTrades
+        .sortedBy { it.roe }
+        .map {
+            "LOSS [${it.pair} - ${it.position}]: ${it.roe}% ROE (Lev: ${it.leverage}x)"
+        }
 }
