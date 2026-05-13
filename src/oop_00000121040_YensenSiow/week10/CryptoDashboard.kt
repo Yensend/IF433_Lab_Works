640c7599d437b803c250a5fc634c19260b7c7e7f
Yensend <siownairui@gmail.com>
2026-05-13 20:07:46 +0700

    week10: (task) populate coin repository

--- src/oop_00000121040_YensenSiow/week10/CryptoDashboard.kt
+++ src/oop_00000121040_YensenSiow/week10/CryptoDashboard.kt
@@ -3,5 +3,8 @@ package oop_00000121040_YensenSiow.week10
 fun main() {
 
     val coinRepo = WalletRepository<Coin>()
+    coinRepo.add(Coin("BTC", 1.25))
+    coinRepo.add(Coin("ETH", 10.5))
+    coinRepo.add(Coin("USDT", 2500.0))
 
 }
